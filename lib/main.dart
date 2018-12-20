3e80cd4a8a777872131689087419f95b767b18dc
mac <mac@Jade-MBP.net.fpt>
2018-12-20 23:34:55 +0700

    create infinite scrolling ListView

--- lib/main.dart
+++ lib/main.dart
@@ -7,18 +7,23 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // final wordPair = new WordPair.random();
+    //   // final wordPair = new WordPair.random();
+    //   return new MaterialApp(
+    //     title: 'Welcome to Flutter',
+    //     home: new Scaffold(
+    //       appBar: new AppBar(
+    //         title: const Text('Welcome to Flutter'),
+    //       ),
+    //       body: new Center(
+    //         // child: new Text(wordPair.asPascalCase),
+    //         child: new RandomWords(),
+    //       ),
+    //     ),
+    //   );
+    // }
     return new MaterialApp(
-      title: 'Welcome to Flutter',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: new Center(
-          // child: new Text(wordPair.asPascalCase),
-          child: new RandomWords(),
-        ),
-      ),
+      title: 'Startup name generator',
+      home: new RandomWords(),
     );
   }
 }
