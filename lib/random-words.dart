d67b7e90a1f13881e6347b2239e4d5d954f7ae12
mac <mac@Jade-MBP.net.fpt>
2018-12-21 00:12:18 +0700

    add interactivity

--- lib/random-words.dart
+++ lib/random-words.dart
@@ -30,12 +30,20 @@ class RandomWordsState extends State<RandomWords> {
   Widget _buildRow(WordPair pair) {
     final bool alreadySaved = _saved.contains(pair);
     return new ListTile(
-      title: new Text(pair.asPascalCase, style: _biggerFont),
-      trailing: new Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-    );
+        title: new Text(pair.asPascalCase, style: _biggerFont),
+        trailing: new Icon(
+          alreadySaved ? Icons.favorite : Icons.favorite_border,
+          color: alreadySaved ? Colors.red : null,
+        ),
+        onTap: () {
+          setState(() {
+            if (alreadySaved) {
+              _saved.remove(pair);
+            } else {
+              _saved.add(pair);
+            }
+          });
+        });
   }
 
   @override
