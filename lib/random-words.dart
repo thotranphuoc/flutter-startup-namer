c9fe32fd22913d7e72ef9602fd1437cda4a51da2
mac <mac@Jade-MBP.net.fpt>
2018-12-21 00:06:36 +0700

    add icon to the list

--- lib/random-words.dart
+++ lib/random-words.dart
@@ -1,25 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
-
-class RandomWords extends StatefulWidget{
+class RandomWords extends StatefulWidget {
   @override
-    RandomWordsState createState() => new RandomWordsState();
+  RandomWordsState createState() => new RandomWordsState();
 }
 
-class RandomWordsState extends State<RandomWords>{
+class RandomWordsState extends State<RandomWords> {
   final List<WordPair> _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = new Set<WordPair>();
   final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
 
-  Widget _buildSuggestions(){
+  Widget _buildSuggestions() {
     return new ListView.builder(
       padding: const EdgeInsets.all(16.0),
-      itemBuilder: (BuildContext _context, int i){
-        if(i.isOdd){
+      itemBuilder: (BuildContext _context, int i) {
+        if (i.isOdd) {
           return new Divider();
         }
-        final int index = i ~/2;
-        if(index >= _suggestions.length){
+        final int index = i ~/ 2;
+        if (index >= _suggestions.length) {
           _suggestions.addAll(generateWordPairs().take(10));
         }
         return _buildRow(_suggestions[index]);
@@ -27,21 +27,25 @@ class RandomWordsState extends State<RandomWords>{
     );
   }
 
-  Widget _buildRow(WordPair pair){
+  Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
     return new ListTile(
-      title: new Text(
-        pair.asPascalCase, style: _biggerFont
+      title: new Text(pair.asPascalCase, style: _biggerFont),
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
       ),
     );
   }
+
   @override
-    Widget build(BuildContext context) {
-      final WordPair wordPair = new WordPair.random();
-      return new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Startup Name Generator'),
-        ),
-        body: _buildSuggestions(),
-      );
-    }
+  Widget build(BuildContext context) {
+    final WordPair wordPair = new WordPair.random();
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
 }
