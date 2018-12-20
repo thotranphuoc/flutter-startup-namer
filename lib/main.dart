b5312d8d73e326bd5cfc7d6ca13091529a07b7c4
mac <mac@Jade-MBP.net.fpt>
2018-12-20 23:13:00 +0700

    init Flutter app add external package: english_words add stateful widget

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+// import 'package:english_words/english_words.dart';
+import './random-words.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // final wordPair = new WordPair.random();
+    return new MaterialApp(
+      title: 'Welcome to Flutter',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: const Text('Welcome to Flutter'),
+        ),
+        body: new Center(
+          // child: new Text(wordPair.asPascalCase),
+          child: new RandomWords(),
+        ),
+      ),
+    );
+  }
+}
